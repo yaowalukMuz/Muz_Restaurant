d5eb84ce085e49c6a3745a463cc33dd255c5797a
yaowamuz <yaowamuz.c@gmail.com>
2016-09-01 16:38:55 +0700

    Create class SQLiteOpenHelper_create constructor_class

--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/muzrestaurant/MySQLiteOpenHelper.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/muzrestaurant/MySQLiteOpenHelper.java
@@ -1,7 +1,35 @@
 package com.hitachi_tstv.yodpanom.yaowaluk.muzrestaurant;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by musz on 9/1/2016.
  */
-public class MySQLiteOpenHelper {
+public class MySQLiteOpenHelper extends SQLiteOpenHelper {
+
+    //Explicit
+    private static final String DATABASE_NAME = "Restaurant.db";
+    private static final int  DATABASE_VERSION = 1;
+    private static final String CREATE_USER_TABLE = "create table userTABLE" + "(_id integer primary key," +
+            "Food text, Source text, Price text);";
+    private static final String CREATE_ORDER_TABLE = "create table orderTABLE" +"(_id integer primary key," +
+            "officer text, Desk text, Food text, Item text);";
+
+
+    public MySQLiteOpenHelper(Context context) {
+        super(context,DATABASE_NAME,null, DATABASE_VERSION);
+
+    }// End Constructor
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
